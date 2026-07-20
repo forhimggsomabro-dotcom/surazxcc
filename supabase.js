14cad59333317f6ee297145caf697e636ff82db3
forhimggsomabro-dotcom <forhimggsomabro@gmail.com>
2026-07-20 23:44:35 +0530

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,33 @@
+import { createClient } from 
+
+"https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+
+
+
+
+const supabaseUrl =
+
+"https://wmlcdishdejcwdrrjxko.supabase.co";
+
+
+
+
+
+const supabaseKey =
+
+"sb_publishable_KiPjqn3bg3n8T9DuW8U95w_kt4xDE4d";
+
+
+
+
+
+export const supabase =
+
+createClient(
+
+    supabaseUrl,
+
+    supabaseKey
+
+);
